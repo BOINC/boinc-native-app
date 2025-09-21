e901a899ab82776ccd4378dfa65dff0905dc3246
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-09-21 17:20:44 +0200

    Add sample project
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

---
+++ bootstrap.bat
@@ -0,0 +1,19 @@
+REM This file is part of BOINC.
+REM https://boinc.berkeley.edu
+REM Copyright (C) 2025 University of California
+REM
+REM BOINC is free software; you can redistribute it and/or modify it
+REM under the terms of the GNU Lesser General Public License
+REM as published by the Free Software Foundation,
+REM either version 3 of the License, or (at your option) any later version.
+REM
+REM BOINC is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+REM See the GNU Lesser General Public License for more details.
+REM
+REM You should have received a copy of the GNU Lesser General Public License
+REM along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+git clone https://github.com/microsoft/vcpkg.git
+vcpkg/bootstrap-vcpkg.bat
